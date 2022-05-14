44483583068945d8b8dbfdcfd280783c2b613128
Germán <GassetGerman@gmail.com>
2022-05-15 01:15:30 +0200

    added function descriptions

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ v = volumeProcessor()
 
 v.set_url('./current_media/Daviles de Novelda - Las calles de oro (Videoclip Oficial).mp4')
 
-v.get_min_max_volume()
+v.set_min_max_volume()
+
+v.get_volume_percentage_from_ms
 
 #print(volume.get_min_max_volume())
--- volume.py
+++ volume.py
@@ -12,6 +12,7 @@ class volumeProcessor:
         self.audio_arr = self.audio.to_soundarray(nbytes=4, fps=self.fps)
 
     def set_min_max_volume(self):
+        """You must use for each media set_url before using this function"""
         min, max = 0, 0
         for frame in range(len(self.audio_arr)):
             frame_audio = self.audio_arr[frame]
@@ -22,12 +23,14 @@ class volumeProcessor:
             self.max = max
 
     def get_volume(self, ms):
+        """You must use for each media set_url before using this function"""
         frame = ms * (self.fps / 1000)
         frame_audio = self.audio_arr[frame]
         sum = frame_audio[0] + frame_audio[1]
         return sum
 
     def get_volume_percentage(self, volume):
+        """You must use for each media set_min_max_volume before using this function"""
         percentage = volume - self.min
         new_max = self.max - self.min
         percentage /= new_max
@@ -38,4 +41,5 @@ class volumeProcessor:
         return percentage
 
     def get_volume_percentage_from_ms(self, ms):
+        """You must use for each media set_min_max_volume before using this function"""
         return self.get_volume_percentage(self.get_volume(ms))
