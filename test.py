c291f2013a980ff2f0759f1f684e5f5474d2bb65
Germán <GassetGerman@gmail.com>
2022-05-21 02:19:40 +0200

    now at test.py there will be a benchmark of seconds to set-up

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 import numpy as np
 from volume import VolumeProcessor
+import time
 
 v = VolumeProcessor(20)
 
+initial_time = time.time()
 v.set_url('./current_media/Daviles de Novelda - Las calles de oro (Videoclip Oficial).mp4')
+print('Seconds to get media object: ', initial_time - time.time())
+initial_time = time.time()
 
 v.set_min_max_volume()
+print('Seconds get min-max volume: ', time.time() - initial_time)
 
 print(v.get_volume_percentage_from_ms(20 * 1000))
-
-#print(volume.get_min_max_volume())
